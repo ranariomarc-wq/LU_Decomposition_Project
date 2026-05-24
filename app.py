4590fa2182a0bc3cb95f3445a97183fbdf3ff376
ranario <ranariomarc@gmail.com>
2026-05-25 04:33:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,74 +4,97 @@ import numpy as np
 app = Flask(__name__)
 
 
-def doolittle_lu(A):
+def lu_doolittle(A):
     n = len(A)
 
     L = np.zeros((n, n))
     U = np.zeros((n, n))
+
+    steps = []
 
     for i in range(n):
         L[i][i] = 1
 
     for j in range(n):
 
-        # Upper Triangular
+        # Compute U
         for i in range(j + 1):
-            sum1 = 0
+
+            sum_val = 0
+
             for k in range(i):
-                sum1 += U[k][j] * L[i][k]
+                sum_val += L[i][k] * U[k][j]
 
-            U[i][j] = A[i][j] - sum1
+            U[i][j] = A[i][j] - sum_val
 
-        # Lower Triangular
+            steps.append(
+                f"U[{i+1}][{j+1}] = {A[i][j]} - {round(sum_val,4)} = {round(U[i][j],4)}"
+            )
+
+        # Compute L
         for i in range(j, n):
-            sum1 = 0
+
+            sum_val = 0
+
             for k in range(j):
-                sum1 += U[k][j] * L[i][k]
+                sum_val += L[i][k] * U[k][j]
 
-            if U[j][j] == 0:
-                raise ValueError("Division by zero encountered!")
+            L[i][j] = (A[i][j] - sum_val) / U[j][j]
 
-            L[i][j] = (A[i][j] - sum1) / U[j][j]
+            steps.append(
+                f"L[{i+1}][{j+1}] = ({A[i][j]} - {round(sum_val,4)}) / {round(U[j][j],4)} = {round(L[i][j],4)}"
+            )
 
-    return L, U
+    return L, U, steps
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
+    size = 3
+    matrix = None
     L = None
     U = None
-    matrix = None
+    steps = []
     error = None
 
     if request.method == "POST":
+
         try:
+
             size = int(request.form["size"])
 
             matrix = []
 
             for i in range(size):
+
                 row = []
+
                 for j in range(size):
+
                     value = float(request.form[f"cell_{i}_{j}"])
+
                     row.append(value)
+
                 matrix.append(row)
 
             A = np.array(matrix)
 
-            L, U = doolittle_lu(A)
+            L, U, steps = lu_doolittle(A)
 
-            L = np.round(L, 4).tolist()
-            U = np.round(U, 4).tolist()
+            L = np.round(L, 4)
+            U = np.round(U, 4)
 
         except Exception as e:
             error = str(e)
 
     return render_template(
         "index.html",
+        size=size,
+        matrix=matrix,
         L=L,
         U=U,
-        matrix=matrix,
+        steps=steps,
         error=error
     )
 
